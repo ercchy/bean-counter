258042faf0c93b5f1508dad255168c96931dce6d
ercchy <erika@serox.si>
2015-09-29 20:03:43 +0200

    Fix jake.sh error
    Fixing a typo in the jake.sh file

--- build/scripts/build.jakefile.js
+++ build/scripts/build.jakefile.js
@@ -1,3 +1,11 @@
 /**
  * Created by erika on 9/29/15.
  */
+(function () {
+    "use strict";
+
+    task("default", function () {
+        console.log("\n\nBUILD OK");
+    });
+
+}());
--- jake.sh
+++ jake.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 [ ! -f node_modules/.bin/jake ] && echo "Building node modules..." && npm rebuild
-node_modules/.bin/jake -f build/scripts/build.jakefile.js %*
+node_modules/.bin/jake -f build/scripts/build.jakefile.js $*
