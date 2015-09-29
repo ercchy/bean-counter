581cdde5cac595b91e960afd601a513b3f0a9e19
ercchy <erika@serox.si>
2015-09-29 20:26:59 +0200

    Check for Node version
    Added Node version check to automated build

--- build/scripts/build.jakefile.js
+++ build/scripts/build.jakefile.js
@@ -4,11 +4,23 @@
 (function () {
     "use strict";
 
+    var version = require("../util/version_checker.js");
+
     var startTime = Date.now();
 
-    task("default", function () {
+    task("default", [ "version" ], function () {
         var elspsedSeconds = (Date.now() - startTime) / 1000;
         console.log("\n\nBUILD OK (" + elspsedSeconds.toFixed(2) + "s)");
     });
+
+    task("version", function() {
+        console.log("Checking Node.js version: .");
+        version.check({
+            name: "Node",
+            expected: require("../../package.json").engines.node,
+            actual: process.version,
+            strict: true
+        }, complete, fail)
+    }, { async: true });
 
 }());
